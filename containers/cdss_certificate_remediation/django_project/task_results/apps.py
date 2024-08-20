93cd3036d3417cbdfad953ae582be4113c36d124
Calvin Remsburg <cremsburg.dev@gmail.com>
2024-08-20 06:03:47 -0500

    Remove `task_results` app
    Deleted the entire `task_results` app including models, serializers, views, migrations, admin configurations, and tests. This removal is likely part of a larger refactor or deprecation plan.

--- containers/cdss_certificate_remediation/django_project/task_results/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TaskResultsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "django_project.task_results"
--- containers/cdss_certificate_remediation/django_project/task_results/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import TaskResult
-
-
-class TaskResultSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TaskResult
-        fields = ["id", "task_id", "status", "result", "created_at", "updated_at"]
-        read_only_fields = ["id", "task_id", "created_at", "updated_at"]
