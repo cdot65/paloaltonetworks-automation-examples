83280cbd4d86f0740627ccdab82c982bd8c6325f
Calvin Remsburg <calvin@cdot.io>
2024-10-03 17:10:43 -0500

    Remove local and production files
    Deleted multiple configuration, environment, and static files related to local and production setups. This cleanup includes Docker configurations, environment settings, and various other project-maintenance scripts, simplifying the project structure.

--- containers/cdss_certificate_remediation/compose/local/postgres/maintenance/_sourced/constants.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# compose/local/postgres/maintenance/_sourced/constansts.sh
-
-
-BACKUP_DIR_PATH='/backups'
-BACKUP_FILE_PREFIX='backup'
--- containers/cdss_certificate_remediation/django_project/health/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "health"
-
-urlpatterns = [
-    path("", views.health_check, name="health"),
-]
